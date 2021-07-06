2ffa17c18a363bf90f6756742f988107496647f7
Luiz Henrique Rios <luizhrios@gmail.com>
2021-07-05 22:39:16 -0300

    Remove Warnings

--- src/main/java/com/tenniscourts/schedules/ScheduleMapper.java
+++ src/main/java/com/tenniscourts/schedules/ScheduleMapper.java
@@ -1,14 +1,17 @@
 package com.tenniscourts.schedules;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ScheduleMapper {
 
     Schedule map(ScheduleDTO source);
 
+    @Mapping(target = "tennisCourtId", source = "tennisCourt.id")
     ScheduleDTO map(Schedule source);
 
     List<ScheduleDTO> map(List<Schedule> source);
